8a3fc293eea2b411d799b68e6902e1a3c7f544a4
sabin411 <sherasabin411@gmail.com>
2023-01-23 22:49:43 +0545

    fix:Renamed the file name.

--- helloworld.go
+++ helloworld.go
@@ -9,6 +9,9 @@ var pl = fmt.Println
 
 
 func main(){
+	// learning variables
+	var vName string = "Sabin shrestha";
+	pl(vName, "is my name!!");
 	pl("What is you name");
 	reader := bufio.NewReader(os.Stdin);
 
