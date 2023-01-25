32b45c270c3572677ceb176dd942a616f8d13d81
sabin411 <sherasabin411@gmail.com>
2023-01-25 20:35:38 +0545

    feat:Played with formatted datatypes.

--- math.go
+++ math.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	"math"
 )
 var pl = fmt.Println
 
@@ -12,6 +13,24 @@ func main(){
 	rand.Seed(seedSecs);
 	randNum := rand.Intn(50) + 1;
 	pl("My favorite number is", randNum);
+	pl("Max value", math.Max(34, 33))
+	pl("Max value", math.Min(323, 2))
 	// Things to keep in mind 
 	// 1. There are a lot of math function that i will rnd when required. 
+
+	// formatted datatypes 
+	// %d - int
+	// %f - float
+	// %t - bool
+	// %s - string
+	// %c - char
+	// %v - value
+	// %T - type
+	// %b - binary
+	// %x - hex
+	// %e - scientific notation
+	// %p - pointer
+	fmt.Printf("%9v %T %b %x %e %p", 3.14, true, "sabin", "#000");
+
+
 }
