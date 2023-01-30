f30b45cc90dba82937eef3c401a54b685b899bba
sabin411 <sherasabin411@gmail.com>
2023-01-30 16:01:20 +0545

    feat: learned array

--- array.go
+++ array.go
@@ -6,5 +6,19 @@ import (
 var pl = fmt.Println
 
 func main(){
-	
+	myArr := [5]int{1, 2, 3, 4, 5}
+	for i, v := range myArr {
+		pl(i, v)
+	}
+	a := [4]int{1, 2, 4, 5}
+	a[3] = 400
+	pl(a)
+
+	var twoD [2][3]int
+    for i := 0; i < 2; i++ {
+        for j := 0; j < 3; j++ {
+            twoD[i][j] = i + j
+        }
+    }
+    fmt.Println("2d: ", twoD)
 }
