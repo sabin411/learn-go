ace190eb98078d5c44274a181a37744f9879d8ef
sabin411 <sherasabin411@gmail.com>
2023-02-06 17:45:26 +0545

    feat:Created jwt authentication login controller.a

--- controllers/usersController.go
+++ controllers/usersController.go
@@ -3,8 +3,12 @@ package controllers
 import (
 	"go-gin/initializers"
 	"go-gin/models"
+	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -16,40 +20,48 @@ func Signup(ctx *gin.Context){
 		Password string 
 	}
 	// 1. Get email and password from the body
-	if ctx.BindJSON(&body) == nil {
+	if ctx.Bind(&body) != nil {
 		ctx.JSON(400, gin.H{"message": "Bad request"})
 		return
 	}
+	println(&body, "flkdjslkfjldskj")
 
-	user := &models.User{Email: body.Email, Password: body.Password}
 	// 2. hash the password. 
-	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
 
 	if err != nil {
 		ctx.JSON(500, gin.H{"message": "Error hashing password"})
 		return
 	}
 	// 3. Create the user. 
-	result := initializers.DB.Create(&models.User{Email: user.Email, Password: string(hash)})
+	result := initializers.DB.Create(&models.User{Email: body.Email, Password: string(hash)})
 	if result.Error != nil {
-		ctx.JSON(500, gin.H{"message": "Error creating user"})
+		ctx.JSON(500, gin.H{"message": "Error creating user" })
 		return
 	}
 
 	// 4. return response. 
 	ctx.JSON(200, gin.H{"message": "User crated successfully"})
-	return;
+
 }
 
 
 func Login(ctx *gin.Context){
+	// Create the JWT key used to create the signature
+	jwtKey := []byte(os.Getenv("SECRET_KEY"));
+
+	type Claims struct {
+		Email string `json:"username"`
+		jwt.RegisteredClaims
+	}
+
 	// 1. Get email and password from the body
 	var loginBody struct {
 		Email string 
 		Password string
 	}
 
-	if ctx.BindJSON(&loginBody) == nil {
+	if ctx.Bind(&loginBody) != nil {
 		ctx.JSON(400, gin.H{"message": "Bad request"})
 		return
 	}
@@ -67,12 +79,35 @@ func Login(ctx *gin.Context){
 	if err != nil {
 		ctx.JSON(404, gin.H{"message": "Email or password is wrong."})
 		return
-	}else{
-		
 	}
 
 	// 4. Generate JWT token
+	// Declare the expiration time of the token
+	// here, we have kept it as 5 minutes
+	expirationTime := time.Now().Add(time.Hour * 24 * 30);
+	
+	claims := &Claims{
+		Email: user.Email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			// In JWT, the expiry time is expressed as unix milliseconds
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
 
+	// Declare the token with the algorithm used for signing, and the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Create the JWT string
+	tokenString, err := token.SignedString(jwtKey)
 
-	// 5. return response
+	if err != nil {
+		// If there is an error in creating the JWT return an internal server error
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Error signing token"})
+		return
+	}
+		
+	// Finally, we set the client cookie for "token" as the JWT we just generated
+	// we also set an expiry time which is the same as the token itself
+	ctx.SetCookie("token", tokenString, int(expirationTime.Unix()), "/", "localhost", false, true)
+
+	ctx.JSON(200, gin.H{"message": "User logged in successfully"})
 }
